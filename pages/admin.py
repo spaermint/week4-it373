e8b2d0ccf9a7d7f69552ddc8b9beb193ec438acb
spaermint <allenlem.claridad@gmail.com>
2025-10-02 20:06:19 +0800

    added dockerfiles and more features for admin previlages

--- pages/admin.py
+++ pages/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
 from pages.models import Post, Comment, Student, Course, Enrollment
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Student)
-admin.site.register(Course)
-admin.site.register(Enrollment)
+class CommentInLine(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_at")
+    search_fields = ("title", "body")
+    list_filter = ("created_at",)
+    ordering = ("-created_at",)
+    inlines = [CommentInLine]               # Attach the inline
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("author", "post", "created_at")
+    search_fields = ("author", "test")
+    list_filter = ("created_at",)
+
+# @admin.register(Student)
+
+# @admin.register(Course)
+
+# @admin.register(Enrollment)
+
+
+#admin.site.register(Enrollment)
